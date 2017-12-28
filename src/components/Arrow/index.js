b54cea7c3f4fe01d560dffad103953cdd80e7c24
nolleto <felipenolletonascimento@gmail.com>
2017-12-28 11:16:59 -0200

    Fix PropTypes 'react' to 'prop-types'

--- src/components/Arrow/index.js
+++ src/components/Arrow/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Arrow extends Component {
   makeControlPoint = (point, n = 50) => {
--- src/components/PropertiesSuperNode/index.js
+++ src/components/PropertiesSuperNode/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { changeNodeId } from '../../actions';
 import { getNodes } from '../../selectors';
